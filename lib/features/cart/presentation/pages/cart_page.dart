df373d415fb597b5e233698c5b5be05d8ab4ecce
AryaPramudya898 <aryapramudya610@gmail.com>
2026-03-04 05:37:32 +0700

    membuat tampilan keranjang menu

--- lib/features/cart/presentation/pages/cart_page.dart
+++ lib/features/cart/presentation/pages/cart_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:week_3_1123150004/features/cart/presentation/providers/cart_provider.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var cart = context.watch<CartProvider>();
+
+    return Scaffold(
+      backgroundColor: Colors.purple[100],
+      appBar: AppBar(
+        title: const Text('Keranjang Menu', style: TextStyle(color: Colors.white,),),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: const Icon(Icons.fastfood),
+                  title: Text(cart.items[index].name),
+                );
+              },
+            ),
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () => cart.removeAll(),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text(
+                'Buang',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
