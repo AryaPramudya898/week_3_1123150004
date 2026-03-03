c7c865e2ed5a10b1fa68a7ea5df20ee19b6c4121
AryaPramudya898 <aryapramudya610@gmail.com>
2026-03-04 05:34:31 +0700

    membuat tampilan catalog

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:week_3_1123150004/core/routes/app_router.dart';
 import 'package:week_3_1123150004/features/cart/domain/entities/product.dart';
+import 'package:week_3_1123150004/features/cart/presentation/widgets/add_button_widget.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
@@ -12,6 +14,28 @@ class CatalogPage extends StatelessWidget {
       Product(id: '3', name: 'Chicken Popcorn', price: '24000'),
       Product(id: '4', name: 'Onion Rings', price: '22000'),
     ];
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.purple[100],
+      appBar: AppBar(
+        title: Text('Catalog', style: TextStyle(color: Colors.white,),),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart, color: Colors.white,),
+            onPressed: () => Navigator.pushNamed(context, AppRouter.cart),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].name),
+            trailing: AddButtonWidget(product: products[index]),
+          );
+        },
+      ),
+    );
   }
 }
