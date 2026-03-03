397a6667e425ede8cf28642d989c898d2dea14b4
AryaPramudya898 <aryapramudya610@gmail.com>
2026-03-04 05:27:38 +0700

    tambah menu

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:week_3_1123150004/features/cart/domain/entities/product.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final products = [
+      Product(id: '1', name: 'Taro Latte', price: '27000'),
+      Product(id: '2', name: 'Red Velvet Latte', price: '29000'),
+      Product(id: '3', name: 'Chicken Popcorn', price: '24000'),
+      Product(id: '4', name: 'Onion Rings', price: '22000'),
+    ];
     return const Placeholder();
   }
 }
